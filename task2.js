6af64d72f76e57072e774dfb39a9a1fdbed90bc7
nahyeon <k92544199@gmail.com>
2023-03-31 22:43:45 +0900

    js기초

--- task2.js
+++ task2.js
@@ -1,1 +1,18 @@
 /* 2번 문제 답안을 작성해주세요. */
+function solution(num_list) {
+    let cnt1 = 0;
+    let cnt2 = 0;
+
+    let answer=num_list;
+    answer.map((num)=>{
+        if(num%2===0){
+            cnt1++;
+        }
+        else{
+            cnt2++;
+        }
+        return num;
+    })
+    
+    return [cnt1, cnt2];
+}
--- task3.js
+++ task3.js
@@ -1,1 +1,10 @@
 /* 3번 문제 답안을 작성해주세요. */
+function solution(n) {
+    let num=n;
+        let sol=1;
+        let cnt=1;
+        while(sol<=num){
+            sol*=cnt;
+            cnt=cnt+1;}
+    return cnt-2;
+}
